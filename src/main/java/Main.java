dcc7384957987cde66956885e6215a8927129d04
cherrydan <cherrydan.1978@gmail.com>
2023-08-12 21:50:13 +0300

    Initial data insertion into DB

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,56 @@
-import busines_logic.Util;
+import entity.Address;
+import entity.EmplProj;
+import entity.Employee;
+import entity.Project;
+import service.AddressService;
+import service.EmplProjService;
+import service.EmployeeService;
+import service.ProjectService;
+
+import java.sql.Date;
+import java.sql.SQLException;
+import java.util.Calendar;
 
 public class Main {
     public static void main(String[] args) {
-        Util util = new Util();
-        util.getConnection();
+        AddressService addressService = new AddressService();
+        EmployeeService employeeService = new EmployeeService();
+        ProjectService projectService = new ProjectService();
+        EmplProjService emplProjService = new EmplProjService();
+
+        Address address = new Address();
+        address.setId(1L);
+        address.setCountry("DC");
+        address.setCity("Gotham City");
+        address.setStreet("Arkham Street, 1");
+        address.setPostCode("12345");
+
+        Employee employee = new Employee();
+        employee.setId(1L);
+        employee.setFirstName("James");
+        employee.setLastName("Gordon");
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(1939, Calendar.MAY, 1);
+        employee.setBirthday(new Date(calendar.getTime().getTime()));
+        employee.setAddressId(address.getId());
+
+        Project project = new Project();
+        project.setId(1L);
+        project.setTitle("Gotham City Police Dept Commissioner");
+
+        EmplProj emplProj = new EmplProj();
+        emplProj.setProjectId(project.getId());
+        emplProj.setEmployeeId(employee.getId());
+
+        try {
+            addressService.add(address);
+            employeeService.add(employee);
+            projectService.add(project);
+            emplProjService.add(emplProj);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
